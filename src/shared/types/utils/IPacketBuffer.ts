43a82829db49635b0ad4c571cb3bec79dce95f72
Teinc3 <teinc3@gmail.com>
2025-07-08 12:10:03 +0100

    Added auto-resizing Packet Buffer

--- src/shared/types/utils/IPacketBuffer.ts
+++ src/shared/types/utils/IPacketBuffer.ts
@@ -1,8 +1,11 @@
-export default interface IByteArray {
+export default interface IPacketBuffer {
 
+    get buffer(): ArrayBuffer;
+    get dataView(): DataView;
     get index(): number;
     set index(newIndex: number);
 
+    read(length: number): Uint8Array;
     readBool(): boolean;
     readByte(): number;
     readShort(): number;
@@ -10,11 +13,14 @@ export default interface IByteArray {
     readFloat(): number;
     readString(): string;
 
+    write(data: ArrayBuffer | Uint8Array, offset?: number): void;
     writeBool(value: boolean, offset?: number): void;
     writeByte(value: number, offset?: number): void;
     writeShort(value: number, offset?: number): void;
     writeInt(value: number, offset?: number): void;
     writeFloat(value: number, offset?: number): void;
     writeString(value: string, offset?: number): void;
+
+    clone(): IPacketBuffer;
 
 }
