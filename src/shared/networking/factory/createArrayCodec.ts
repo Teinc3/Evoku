ea27c4099af41afb502a2b520c5f780ba86233e8
Teinc3 <s17415@lsc.hk>
2025-07-19 01:20:41 +0100

    Added dynamic array codec

---
+++ src/shared/networking/factory/createArrayCodec.ts
@@ -0,0 +1,36 @@
+import AbstractCodec from "../codecs/AbstractCodec";
+import type IPacketBuffer from "../../types/utils/IPacketBuffer";
+import type { CodecConstructor } from "../../types/networking/ICodec";
+
+
+/**
+ * Factory function to create an ArrayCodec class with a specific element codec.
+ */
+export function createArrayCodec<DType>(elementCodec: CodecConstructor<DType>) {
+    return class ArrayCodec extends AbstractCodec<DType[]> {
+        public readonly Codec = elementCodec;
+        public readonly elementCodec: AbstractCodec<DType>;
+
+        constructor() {
+            super();
+            this.elementCodec = new elementCodec();
+        }
+
+        encode(buffer: IPacketBuffer, data: DType[]): number {
+            let bytes = buffer.writeInt(data.length); // Write array length
+            for (const item of data) {
+                bytes += this.elementCodec.encode(buffer, item);
+            }
+            return bytes;
+        }
+
+        decode(buffer: IPacketBuffer): DType[] {
+            const length = buffer.readInt();
+            const arr: DType[] = [];
+            for (let i = 0; i < length; i++) {
+                arr.push(this.elementCodec.decode(buffer));
+            }
+            return arr;
+        }
+    };
+}
