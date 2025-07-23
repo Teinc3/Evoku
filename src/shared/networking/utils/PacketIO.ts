d32c2edb2c22e43161978e6990c7bd942bd33cbc
Teinc3 <teinc3@gmail.com>
2025-07-23 11:40:20 +0100

    Updated PacketIO Implementation as a stateful class with security API

--- src/shared/networking/utils/PacketIO.ts
+++ src/shared/networking/utils/PacketIO.ts
@@ -4,37 +4,51 @@ import ActionCodec from "../codecs/custom/ActionCodec";
 
 import type ActionEnum from "../../types/enums/actions";
 import type ActionMap from "../../types/actionmap";
+// import type { SecurityModule } from "../security/SecurityModule";
 
+export default class PacketIO {
+  // private security?: SecurityModule;
 
-export function decodePacket(buffer: ArrayBuffer) {
-  const packetBuffer = new PacketBuffer(buffer.byteLength);
-  packetBuffer.write(buffer);
+  // constructor(security?: SecurityModule) {
+  //   this.security = security;
+  // }
 
-  const action = (new ActionCodec).decode(packetBuffer);
-  const Packet = PacketRegistry.getPacket(action);
-  if (!Packet) {
-    throw new Error(`No packet registered for action: ${action}`);
+  decodePacket(buffer: ArrayBuffer) {
+    // let dataBuffer = buffer;
+    // if (this.security) {
+    //   dataBuffer = this.security.decrypt(buffer);
+    // }
+    const packetBuffer = new PacketBuffer(buffer.byteLength);
+    packetBuffer.write(buffer);
+
+    const action = (new ActionCodec).decode(packetBuffer);
+    const Packet = PacketRegistry.getPacket(action);
+    if (!Packet) {
+      throw new Error(`No packet registered for action: ${action}`);
+    }
+
+    // Reset index to allow packet codec to read the complete thing
+    packetBuffer.index = 0;
+    const data = (new Packet).unwrap(packetBuffer);
+    return data;
   }
 
-  // Reset index to allow packet codec to read the complete thing
-  packetBuffer.index = 0;
-  const data = (new Packet).unwrap(packetBuffer);
-  return { action, data };
-}
+  encodePacket<
+    GenericAction extends ActionEnum,
+    GenericContract extends ActionMap[GenericAction]
+  >(action: GenericAction, dataContract: GenericContract) {
+    const Packet = PacketRegistry.getPacket(action);
+    if (!Packet) {
+      throw new Error(`No packet registered for action: ${action}`);
+    }
 
-export function encodePacket<
-  GenericAction extends ActionEnum,
-  GenericContract extends ActionMap[GenericAction]
->(action: GenericAction, dataContract: GenericContract) {
-
-  const Packet = PacketRegistry.getPacket(action);
-  if (!Packet) {
-    throw new Error(`No packet registered for action: ${action}`);
+    let buffer = new Packet({
+      action,
+      ...dataContract
+    }).wrap().buffer;
+    // if (this.security) {
+    //   buffer = this.security.encrypt(buffer);
+    // }
+    return buffer;
   }
-
-  return new Packet({
-    action,
-    ...dataContract
-  }).wrap().buffer;
-
 }
