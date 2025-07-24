34b52df1023cf1d2c759beb8af74e88f9ed58da5
Teinc3 <teinc3@gmail.com>
2025-07-24 20:31:52 +0100

    Added Room and Session Models and SessionManager

---
+++ src/server/managers/SessionManager.ts
@@ -0,0 +1,35 @@
+import SessionModel from "../models/Session";
+
+import type { UUID } from "crypto";
+import type ServerSocket from "../models/ServerSocket";
+
+
+/**
+ * A global Session manager that handles all user sessions in game.
+ */
+export default class SessionManager {
+    constructor (
+        private sessions: Map<UUID, SessionModel> = new Map()
+    ) {}
+
+    public createSession(socket: ServerSocket): SessionModel {
+        const session = new SessionModel(
+            socket,
+            () => this.removeSession(session.uuid)
+        );
+        this.sessions.set(session.uuid, session);
+        return session;
+    }
+
+    public getSession(uuid: UUID): SessionModel | undefined {
+        return this.sessions.get(uuid);
+    }
+
+    public removeSession(uuid: UUID): void {
+        const session = this.sessions.get(uuid);
+        if (session) {
+            session.destroy(true);
+            this.sessions.delete(uuid);
+        }
+    }
+}
---
+++ src/server/models/Session.ts
@@ -0,0 +1,78 @@
+import { randomUUID, type UUID } from "crypto";
+
+import ServerSocket from "./ServerSocket";
+
+import type RoomModel from "./Room";
+
+
+/**
+ * A model representing a user session in the game.
+ * 
+ * @param socket - The WebSocket connection for this session.
+ * @readonly @optional @param uuid - Unique identifier for the session.
+ * @optional @param room - The room this session is currently in.
+ */
+export default class SessionModel {
+
+    constructor(
+        public socket: ServerSocket | null, // Require a Socket to be initialised
+        private readonly onDestroy: () => void,
+        public readonly uuid: UUID = randomUUID(),
+        // Optional account info, when the user authenticates using token or in game
+        // private accountInfo: AccountInfo | null = null,
+        public room: RoomModel | null = null,
+        private reconnectionTimer: NodeJS.Timeout | null = null
+    ) {
+        this.uuid = uuid;
+        this.room = room;
+        this.onDestroy = onDestroy.bind(this);
+    }
+
+    /**
+     * Handler when the socket of this session disconnects.
+     * Creates a 2 minute reconnection timer, after which,
+     * the session is permanently removed from the server.
+     */
+    public onDisconnect(): void {
+        this.socket = null; // Clear the socket reference
+        this.reconnectionTimer = setTimeout(() => {
+            this.destroy(); // Destroy the session first
+        }, 2 * 60 * 1000);
+    }
+
+    /**
+     * Handler when the socket of this session reconnects.
+     * Clears the reconnection timer and references the new socket.
+     */
+    public onReconnect(socket: ServerSocket): void {
+        // Plug in the new socket
+        this.socket = socket;
+
+        // Clear the reconnection timer if it exists
+        if (this.reconnectionTimer) {
+            clearTimeout(this.reconnectionTimer);
+            this.reconnectionTimer = null;
+        }
+    }
+
+    public destroy(calledFromManager: boolean = false): void {
+        // Clear the reconnection timer if it exists
+        if (this.reconnectionTimer) {
+            clearTimeout(this.reconnectionTimer);
+            this.reconnectionTimer = null;
+        }
+
+        // Remove all references
+        if (this.room) {
+            this.room.removeSession(this);
+            this.room = null;
+        }
+        this.socket = null;
+
+        // If called from the SessionManager, do not call onDestroy
+        if (!calledFromManager) {
+            this.onDestroy();
+        }
+    }
+
+}
