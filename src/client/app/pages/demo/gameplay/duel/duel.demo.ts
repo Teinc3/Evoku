3b53c038da1f529b2700b7e1c5350f0aeb54651b
Teinc3 <teinc3@gmail.com>
2025-10-10 17:25:46 +0100

    Refactor duel HUD layout

--- src/client/app/pages/demo/gameplay/duel/duel.demo.ts
+++ src/client/app/pages/demo/gameplay/duel/duel.demo.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import AppView from '../../../../types/app-view.enum';
 import ViewStateService from '../../../../services/view-state.service';
 import DuelHudTopComponent from '../../../../components/hud/duel-hud-top/duel-hud-top';
+import BoardModelComponent from '../../../../components/board/board.component';
 
 
 @Component({
   selector: 'app-demo-duel',
   standalone: true,
-  imports: [DuelHudTopComponent],
+  imports: [DuelHudTopComponent, BoardModelComponent],
   templateUrl: './duel.demo.html',
   styleUrl: './duel.demo.scss'
 })
