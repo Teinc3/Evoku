5f97c1627432c8a07d33272117677c6740c7594f
Teinc3 <teinc3@gmail.com>
2025-10-11 09:16:26 +0100

    Added board layout for duel demo

--- src/client/app/pages/demo/gameplay/duel/duel.demo.ts
+++ src/client/app/pages/demo/gameplay/duel/duel.demo.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 
-import AppView from '../../../../types/app-view.enum';
-import ViewStateService from '../../../../services/view-state.service';
+import UniversalProgressBarComponent 
+  from '../../../../components/hud/universal-progress-bar/universal-progress-bar.component';
 import DuelHudTopComponent from '../../../../components/hud/duel-hud-top/duel-hud-top';
+import UtilityButtonsHolderComponent 
+  from '../../../../components/controls/utility-buttons-holder/utility-buttons-holder.component';
+import NumericButtonsHolderComponent 
+  from '../../../../components/controls/numeric-buttons-holder/numeric-buttons-holder.component';
 import BoardModelComponent from '../../../../components/board/board.component';
 
 
 @Component({
   selector: 'app-demo-duel',
   standalone: true,
-  imports: [DuelHudTopComponent, BoardModelComponent],
+  imports: [
+    DuelHudTopComponent, 
+    BoardModelComponent, 
+    UniversalProgressBarComponent, 
+    UtilityButtonsHolderComponent, 
+    NumericButtonsHolderComponent
+  ],
   templateUrl: './duel.demo.html',
   styleUrl: './duel.demo.scss'
 })
 export default class DuelDemoPageComponent {
-  protected readonly AppView = AppView;
-
-  constructor(protected readonly viewStateService: ViewStateService) {}
+  //@ViewChild()
 }
