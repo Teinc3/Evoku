e102ad31bd91f6a70eb9e2d9b75aac157429f284
Teinc3 <teinc3@gmail.com>
2025-10-16 23:52:39 +0100

    Add NotFound page with routing and styling for 404 errors

--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -1,4 +1,5 @@
 import VisionPageComponent from './pages/vision/vision';
+import NotFoundPageComponent from './pages/not-found/not-found';
 import HomePageComponent from './pages/home/home';
 import DemoPageComponent from './pages/demo/demo';
 
@@ -8,5 +9,6 @@ import type { Routes } from '@angular/router';
 export default [
   { path: '', component: HomePageComponent },
   { path: 'demo', component: DemoPageComponent },
-  { path: 'vision', component: VisionPageComponent }
+  { path: 'vision', component: VisionPageComponent },
+  { path: '**', component: NotFoundPageComponent } // Show 404 page for unknown routes
 ] as Routes;
