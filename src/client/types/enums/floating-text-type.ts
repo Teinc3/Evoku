3307e423d672fe0a6cf79a31f45418329322a059
viktorexe <developer.viktorexe@gmail.com>
2025-12-12 23:20:10 +0530

    initial changes, debug remains

---
+++ src/client/types/enums/floating-text-type.ts
@@ -0,0 +1,10 @@
+/** Enum for floating text outcome types */
+enum FloatingTextType {
+  REFLECTED = 'REFLECTED',
+  SHATTERED = 'SHATTERED',
+  FROZEN = 'FROZEN',
+  LOCKED = 'LOCKED'
+}
+
+
+export default FloatingTextType;
--- src/client/types/enums/index.ts
+++ src/client/types/enums/index.ts
@@ -1,3 +1,5 @@
 export { default as AppView } from './app-view';
-export { default as PUPOrbState } from './pup-orb-state';
 export { default as CursorDirectionEnum } from './cursor-direction';
+export { default as DefuseType } from './defuse-type';
+export { default as FloatingTextType } from './floating-text-type';
+export { default as PUPOrbState } from './pup-orb-state';
